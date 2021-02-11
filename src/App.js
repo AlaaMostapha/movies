a9f5ef8213a0124714e95918f981908ebdda9d67
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-11 23:43:06 +0200

    handle loader in interceptor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/index";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Loader from "./components/loader/Loader";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.scss";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Loader className="loader display_none" id="loader" />
         <Router>
           <Routes />
         </Router>
