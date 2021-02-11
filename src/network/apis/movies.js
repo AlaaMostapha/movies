4dd75304818e37e101c335e682e585a6a62551ed
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-11 23:44:49 +0200

    change api from lates to poular

--- src/network/apis/movies.js
+++ src/network/apis/movies.js
@@ -2,7 +2,7 @@ import axiosInstance from "../index";
 //https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
 export const getLastestMovies = async () => {
   console.log("api test");
-  return await axiosInstance.get("/movie/latest?", {
+  return await axiosInstance.get("/movie/popular?", {
     handlerEnabled: true,
   });
 };
