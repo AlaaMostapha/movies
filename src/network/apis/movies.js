8a3f8eaae92014f108eef5cf65fbe8e77bd301e7
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-14 16:49:46 +0200

    remove commented code

--- src/network/apis/movies.js
+++ src/network/apis/movies.js
@@ -1,7 +1,6 @@
 import axiosInstance from "../index";
 //https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
 export const getLastestMovies = async () => {
-  console.log("api test");
   return await axiosInstance.get("/movie/popular?", {
     handlerEnabled: true,
   });
