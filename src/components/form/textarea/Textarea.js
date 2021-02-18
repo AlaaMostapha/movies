dcac6ab7c9b166743bfbf7051f297cbe9ef0d037
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-18 19:46:15 +0200

    add textarea & handle char counter

---
+++ src/components/form/textarea/Textarea.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useController } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import Error from "../error/Error";
+const Textarea = (props) => {
+  const { name, type, errors, label, ...rest } = props;
+  const { field } = useController(props);
+  let [charCount, setCharCount] = useState(0);
+  const countChars = (e) => {
+    setCharCount(e.target.value.length);
+  };
+  return (
+    <div>
+      <div>
+        <label htmlFor={name}>{label}</label>
+      </div>
+      <textarea
+        {...rest}
+        name={name}
+        {...field}
+        rows="5"
+        cols="30"
+        onKeyUp={(e) => countChars(e)}
+      ></textarea>
+      <p className="text-right mb-0">{charCount}/500</p>
+      <ErrorMessage name={name} errors={errors} as={<Error />} />
+    </div>
+  );
+};
+
+export default Textarea;
