383ac539007554e04e3584de4002ff3f4fdd3fa4
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-18 19:47:36 +0200

    add new movi page route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import MoviesList from "../pages/moviesList/MoviesList";
 import MovieDetails from "../pages/movieDetails/MovieDetails";
+import AddNewMovie from "../pages/addNewMovie/AddNewMovie";
 export default function Routes() {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path={["/", "/movies"]} component={MoviesList} />
         <Route path="/movies/:movieId" component={MovieDetails} />
+        <Route path="/AddNewMovie" component={AddNewMovie} />
       </Switch>
     </Router>
   );
