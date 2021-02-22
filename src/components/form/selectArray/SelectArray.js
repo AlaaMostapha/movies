846fbdf59827f39e727019903e6a9d753a157833
AlaaMostapha <alaamoustapha21@gmail.com>
2021-02-22 18:13:52 +0200

    add defaultvalue to select and enhance ui

--- src/components/form/selectArray/SelectArray.js
+++ src/components/form/selectArray/SelectArray.js
@@ -3,7 +3,16 @@ import { useFieldArray, Controller } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import Error from "../error/Error";
 const SelectArray = (props) => {
-  const { name, type, errors, label, options, control, ...rest } = props;
+  const {
+    name,
+    type,
+    errors,
+    label,
+    options,
+    control,
+    defaultValue,
+    ...rest
+  } = props;
   //  const { field } = useController(props);
   const { fields, remove, insert, append } = useFieldArray({
     control,
@@ -17,10 +26,11 @@ const SelectArray = (props) => {
         return (
           <div className="mb-2" key={item.id}>
             <Controller
+              defaultValue={options[0]?.value ? options[0].value : "Action"}
               render={({ value, onChange, onBlur }) => {
                 return (
                   <select
-                    className="col-7 mr-2"
+                    className="col-9 mr-2"
                     value={value}
                     onChange={(e) => {
                       onChange(e.target.value);
@@ -39,7 +49,7 @@ const SelectArray = (props) => {
             />
             {index === 0 && (
               <button
-                className="col-2 mr-2"
+                className="col-2 mr-2 actionBtn"
                 type="button"
                 onClick={() => append("")}
               >
@@ -48,7 +58,7 @@ const SelectArray = (props) => {
             )}
             {index > 0 && (
               <button
-                className="col-2 mr-2"
+                className="col-2 mr-2 actionBtn"
                 type="button"
                 onClick={() => remove(index)}
               >
